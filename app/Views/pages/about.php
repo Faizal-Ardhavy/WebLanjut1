044d9944d8eea2fcc86ce9ef5396dcb6452f0970
Faizal-Ardhavy <79374894+Faizal-Ardhavy@users.noreply.github.com>
2022-08-30 11:00:14 +0700

    push tugas 1

---
+++ app/Views/pages/about.php
@@ -0,0 +1,3 @@
+<h3>ngantuk sumpah</h3>
+<p>mau tidooooooooooorrrrrrrrrrrrrrrrrrrrrrrrrr</p>
+<p>cok ngantuk</p>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,3 @@
+<h3>sayaaaa</h3>
+<p>nama sayaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+<p>ini home btw</p>
